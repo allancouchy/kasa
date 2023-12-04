8a430d81ef2cbddb311332af626f9317e4db477c
allancouchy <m.allancouchy@gmail.com>
2023-12-04 19:20:17 +0100

    add new route

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,26 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { GlobalStyle } from './GlobalStyle'
 
-import Header from './components/Header'
 import Home from './pages/Home'
 import Housing from './pages/Housing'
 import About from './pages/About'
 import Error from './pages/Error'
-import Footer from './components/Footer'
+import Layout from './components/Layout'
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Header />
       <Routes>
-        <Route index element={<Home />} />
-        <Route path='housings/:id' element={<Housing />} />
-        <Route path='about' element={<About />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='housings/:id' element={<Housing />} />
+          <Route path='about' element={<About />} />
 
-        {/* catch all */}
-        <Route path='/*' element={<Error />} />
+          {/* catch all */}
+          <Route path='/*' element={<Error />} />
+        </Route>
       </Routes>
-      <Footer />
     </>
   )
 }
